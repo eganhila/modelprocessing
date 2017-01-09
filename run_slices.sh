5911e909c9c14d60b9fec59d8481d6e72d4ca2fe
Hilary Egan <hilaryye@gmail.com>
2017-01-09 10:45:56 -0700

    added run slices bash script

---
+++ run_slices.sh
@@ -0,0 +1,13 @@
+python sliceplot.py --infile /Volumes/triton/Data/ModelChallenge/SDC_Archive/BATSRUS/3d__ful_4_n00060000_APHmax-SSLONG0.h5 --field all_ion
+python sliceplot.py --infile /Volumes/triton/Data/ModelChallenge/SDC_Archive/BATSRUS/3d__ful_4_n00060000_APHmax-SSLONG180.h5 --field all_ion
+python sliceplot.py --infile /Volumes/triton/Data/ModelChallenge/SDC_Archive/BATSRUS/3d__ful_4_n00060000_APHmax-SSLONG270.h5 --field all_ion
+python sliceplot.py --infile /Volumes/triton/Data/ModelChallenge/SDC_Archive/BATSRUS/3d__ful_4_n00060000_APHmin-SSLONG0.h5 --field all_ion
+python sliceplot.py --infile /Volumes/triton/Data/ModelChallenge/SDC_Archive/BATSRUS/3d__ful_4_n00060000_APHmin-SSLONG180.h5 --field all_ion
+python sliceplot.py --infile /Volumes/triton/Data/ModelChallenge/SDC_Archive/BATSRUS/3d__ful_4_n00060000_APHmin-SSLONG270.h5 --field all_ion
+python sliceplot.py --infile /Volumes/triton/Data/ModelChallenge/SDC_Archive/BATSRUS/3d__ful_4_n00060000_PERmax-SSLONG0.h5 --field all_ion
+python sliceplot.py --infile /Volumes/triton/Data/ModelChallenge/SDC_Archive/BATSRUS/3d__ful_4_n00060000_PERmax-SSLONG180.h5 --field all_ion
+python sliceplot.py --infile /Volumes/triton/Data/ModelChallenge/SDC_Archive/BATSRUS/3d__ful_4_n00060000_PERmax-SSLONG270.h5 --field all_ion
+python sliceplot.py --infile /Volumes/triton/Data/ModelChallenge/SDC_Archive/BATSRUS/3d__ful_4_n00060000_PERmin-SSLONG0.h5 --field all_ion
+python sliceplot.py --infile /Volumes/triton/Data/ModelChallenge/SDC_Archive/BATSRUS/3d__ful_4_n00060000_PERmin-SSLONG180.h5 --field all_ion
+python sliceplot.py --infile /Volumes/triton/Data/ModelChallenge/SDC_Archive/BATSRUS/3d__ful_4_n00060000_PERmin-SSLONG270.h5 --field all_ion
+
