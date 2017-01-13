a5a32e2f566e77e272c5e908e94eef6bdd3c9c90
Hilary Egan <hilaryye@gmail.com>
2017-01-13 13:58:00 -0700

    Streamlined tecplot to work w/ commandline arguments and also to proccess the headers instead of requiring manually editing the file

--- convert_tecplot.py
+++ convert_tecplot.py
@@ -1,88 +1,113 @@
+"""
+Converts an ascii tecplot file into an hdf5 file. Also
+changes field names to be consistent across all file 
+formats. 
+
+Inputs:
+    --outname (-o): filename to save h5 output to. Defaults to
+                    the infile name but extension changed to *.h5
+    --infile (-i): filename to convert
+    --test (-t): Doesn't do anything yet
+"""
+
 import numpy as np
 import h5py 
 import spiceypy as sp
 import glob
+import sys
+import getopt
+from general_functions import *
 
 
+data_conversion = {'H_p1_number_density':lambda x: x/1.00794, 
+                   'O2_p1_number_density':lambda x: x/(2*15.9994), 
+                   'O_p1_number_density':lambda x: x/15.9994, 
+                   'CO2_p1_number_density':lambda x: x/(15.9994*2+12.0107)}
+f_var_rename = 'Misc/name_conversion.txt'
 
 
-name_conversion = {'x':'x', 'y':'y', 'z':'z',
-                  'r':'number_density', 'vx':'velocity_x', 'vy':'velocity_y', 'vz':'velocity_z',
-                  'bx':'magnetic_field_x', 'by':'magnetic_field_y', 'bz':'magnetic_field_z',
-                  'p':'pressure', 'rhp':'H_p1_number_density', 'uxh':'H_p1_velocity_x', 'uyh':'H_p1_velocity_y', 'uzh':'H_p1_velocity_z',
-                  'php':'H_p1_pressure', 'rop2':'O2_p1_number_density', 'uxo2':'O2_p1_velocity_x', 'uyo2':'O2_p1_velocity_y', 'uzo2':'O2_p1_velocity_z',
-                  'po2':'O2_p1_pressure', 'rop':'O_p1_number_density', 'uxo':'O_p1_velocity_x', 'uyo':'O_p1_velocity_y', 'uzo':'O_p1_velocity_z',
-                  'pop':'O_p1_pressure', 'rco2':'CO2_p1_number_density', 'uxco2':'CO2_p1_velocity_x', 'uyco2':'CO2_p1_velocity_y','uzco2':'CO2_p1_velocity_z',
-                  'pco2p':'CO2_p1_pressure',
-                  'b1x':'bx1', 'b1y':'by1', 'b1z':'bz1', 'E':'electric_field_magnitude',
-                  'jx':'current_x', 'jy':'current_y', 'jz':'current_z',
-                  'altitude':'altitude', 'latitude':'latitude', 'longitude':'longitude'}
+def convert_file(fname, h5_name):
 
-data_conversion = {'rhp':lambda x: x/1.00794, 
-                   'rop2':lambda x: x/(2*15.9994), 
-                   'rop':lambda x: x/15.9994, 
-                   'rco2':lambda x: x/(15.9994*2+12.0107)}
+    # read in file
+    with file(fname) as dat_file: 
+        
+        #Process header inforation
+        read_header = True
+        dat_vars = []
+        while read_header:
+            l = dat_file.readline()
+            
+            # Grab the first variable thats at the end of the line
+            if "VARIABLES" in l: dat_vars.append(l.replace("VARIABLES = ", ''))
+            
+            # If the line begins with " then its another variable
+            if l[0] == '"': dat_vars.append(l)
 
+            # Contains the number of elements to read
+            if "Nodes" in l: N = int( (l.split(',')[0]).split("=")[-1] )
 
-def convert_file(fdir, fname):
+            # Begining of last line before data
+            if "DT" in l: read_header = False
 
-    # Number nodes
-    #N = 6292800 
-    #Nskip = 64
-    #dat_vars = ['x', 'y', 'z', 'r', 'vx', 'vy', 'vz', 'bx', 'by', 'bz', 'p', 'rhp',
-    #            'uxh', 'uyh', 'uzh', 'php', 'rop2', 'uxo2', 'uyo2', 'uzo2', 'po2',
-    #            'rop', 'uxo', 'uyo', 'uzo', 'pop', 'rco2', 'uxco2', 'uyco2', 'uzco2',
-    #            'pco2p', 'b1x', 'b1y', 'b1z', 'E', 'jx', 'jy', 'jz']
-    N = 6595200
-    Nskip = 44
-    dat_vars =["x", "y", "z", 'r', 'rhp', 'rop2', 'rop', 'rco2', 'vx', 'vy', 'vz', 'bx', 'by', 'bz', 'p', 'jx', 'jy', 'jz']
+        dat_vars = [v.replace('"', '').replace('\n','') for v in dat_vars]
 
+        # Setup empty data structure
+        data = {var:np.empty(N, dtype=float) for var in dat_vars}
 
-    data = {var:np.empty(N, dtype=float) for var in dat_vars}
-
-    dat_file = file(fdir+fname)
-    i = -1
-    for line in dat_file:
-        i+=1
-        line_dat = line.split(' ')
-        if i < Nskip: continue
-        if i< N+Nskip:
+        # Iterate through the data
+        for i, line in enumerate(dat_file):
+            line_dat = line.split(' ')
             for j, key in enumerate(dat_vars):
-                data[key][i-Nskip] = float(line_dat[j+1])
-        else:
-            break
+                data[key][i] = float(line_dat[j+1])
+
+            # There are bonus garbage lines at the end so we have to
+            # manually exit the loop
+            if i == N-1: break
+
                 
-    dat_file.close()
 
-    x,y,z = data['x'], data['y'], data['z']
+    # Going to make the lat/lon/alt fields
+    lat, lon, alt = convert_coords_cart_sphere(
+            np.array([data['X [R]'], data['Y [R]'], data['Z [R]']]))
 
-    #lat = -1*(np.arctan2(np.sqrt(x**2+y**2), z)*180/np.pi)+90  #theta
-    #lon = np.arctan2(y, x)*180/np.pi   #phi
-    #alt = (np.sqrt(x**2+y**2+z**2)-1)
 
-    #data['latitude'] = lat
-    #data['longitude'] = lon
-    #data['altitude'] = alt
+    # Set up name conversion dictionary
+    name_conversion = {}
+    for pair in file(f_var_rename):
+        k,v = pair.split(',')
+        name_conversion[k] = v[:-1] #remove newline
 
-    h5name = fname.split('.')[0]+'.h5'
-
-    with h5py.File(fdir+h5name, 'w') as f:
+    # Save data
+    with h5py.File(h5_name, 'w') as f:
         for k, v in data.items():
-            #if k in data_conversion.keys():
-            #    v = data_conversion[k](v)
+            if name_conversion[k] in data_conversion.keys():
+                v = data_conversion[name_conversion[k]](v)
             f.create_dataset(name_conversion[k], data=v)
 
 
-def main():
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "i:t:o:", ["infile=","test","outname="])
+    except getopt.GetoptError:
+        return
 
-    #fdir = '/Volumes/triton/Data/ModelChallenge/SDC_Archive/BATSRUS/'
-    #fnames = glob.glob(fdir+'*.dat')
-    fdir = '/Volumes/triton/Data/ModelChallenge/R2349/BATSRUS/'
-    fnames = ['batsrus_3d_multi_fluid.dat']
+    test = False
+    outname = ""
+    fname = ""
 
-    for fname in fnames:
-        print fname
-        convert_file(fdir, fname.split('/')[-1])
+    for opt, arg in opts:
+        if opt in ("-t", "--test"):
+            test = True
+        elif opt in ("-i", "--infile"):
+            fname = arg
+        elif opt in ("-o", "--outname"):
+            outname = arg
 
-if __name__ == '__main__':
-    main()
+    if outname == "": outname = fname.split('.')[0]+'.h5'
+
+    convert_file(fname, outname)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+
