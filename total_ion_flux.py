8141600c63ff65fc549bac36dfd8ac68dd7fc833
Hilary Egan <hilaryye@gmail.com>
2017-01-25 10:27:52 -0700

    added heliosares

--- total_ion_flux.py
+++ total_ion_flux.py
@@ -13,13 +13,18 @@ df = pd.read_csv('Output/sphere_flux_batsrus_multi_species.csv')
 df = df.sort_values('Unnamed: 0')
 df_mspecies = df.set_index('Unnamed: 0')
 
-dfs = {'fluid':df_mfluid, 'species':df_mspecies}
-ls = {'fluid':'o-', 'species':'o--'}
+df = pd.read_csv('Output/Sphere_flux_helio_60km.csv')
+df = df.sort_values('Unnamed: 0')
+df_helio = df.set_index('Unnamed: 0')
+
+dfs = {'fluid':df_mfluid, 'species':df_mspecies, 'helio':df_helio}
+ls = {'fluid':'o-', 'species':'o--', 'helio':'o:'}
 color = {'H_p1':'DarkBlue', 'O2_p1':'DarkSlateBlue', 'O_p1':'Purple', 'CO2_p1':'MediumVioletRed'}
 
 
-for dsk in ['fluid', 'species']:
+for dsk in ['fluid', 'species', 'helio']:
     for ion in [ 'O2_p1', 'O_p1', 'CO2_p1']:
+        if dsk == 'helio' and ion == 'CO2_p1': continue
         plt.plot(dfs[dsk][ion].index, dfs[dsk][ion].values, ls[dsk], color=color[ion],label=ion.replace('_', ''))
 
     if dsk == 'fluid':
@@ -32,11 +37,12 @@ leg1 = plt.legend(handles, labels, loc='upper left')
 plt.gca().add_artist(leg1)
 
 handles = []
-for dsk in ['fluid', 'species']:
+for dsk in ['fluid', 'species', 'helio']:
     line = mlines.Line2D([], [], color='k', ls=ls[dsk][1:], label=dsk)
     handles.append(line)
 
 plt.legend(handles=handles, loc='upper right')
+plt.ylim(1e22, 1e25)
 
 
 plt.savefig('Output/total_ion_flux.pdf')
