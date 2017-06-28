0edaedef4e346459e7570512d35f13bffb9c319c
Hilary Egan <hilaryye@gmail.com>
2017-06-28 11:32:38 -0600

    new models and limits

--- total_ion_flux.py
+++ total_ion_flux.py
@@ -14,11 +14,11 @@ df = pd.read_csv('Output/sphere_flux_batsrus_3d_multi_species.csv')
 df = df.sort_values('Unnamed: 0')
 df_mspecies = df.set_index('Unnamed: 0')
 
-#df = pd.read_csv('Output/sphere_flux_batsrus_pe.csv')
-#df = df.sort_values('Unnamed: 0')
-#df_helio = df.set_index('Unnamed: 0')
+df = pd.read_csv('Output/sphere_flux_batsrus_3d_pe.csv')
+df = df.sort_values('Unnamed: 0')
+df_pe = df.set_index('Unnamed: 0')
 
-df = pd.read_csv('Output/sphere_flux_helio_temp.csv')
+df = pd.read_csv('Output/sphere_flux_heliosares_multi.csv')
 df = df.sort_values('Unnamed: 0')
 df_helio = df.set_index('Unnamed: 0')
 
@@ -26,13 +26,13 @@ df = pd.read_csv('Output/sphere_flux_rhybrid.csv')
 df = df.sort_values('Unnamed: 0')
 df_rhybrid = df.set_index('Unnamed: 0')
 
-dfs = {'fluid':df_mfluid, 'species':df_mspecies, 'helio':df_helio, 'rhybrid':df_rhybrid}
-colors = {'fluid':'DodgerBlue', 'species':'LightSkyBlue', 'helio':'MediumVioletRed', 'rhybrid':'Orchid'}
+dfs = {'fluid':df_mfluid, 'species':df_mspecies, 'helio':df_helio, 'rhybrid':df_rhybrid, 'pe':df_pe}
+colors = {'fluid':'DodgerBlue', 'species':'LightSkyBlue', 'helio':'MediumVioletRed', 'rhybrid':'Orchid', 'pe':'LightSeaGreen'}
 '$\mathrm{Ion\;Flux}$'
 
 f, axes = plt.subplots(2,1)
 for ax, ion in zip(axes, [ 'O2_p1', 'O_p1']):
-    for dsk in ['fluid', 'species', 'helio', 'rhybrid']:
+    for dsk in ['fluid', 'species', 'helio', 'rhybrid', 'pe']:
         ax.plot(dfs[dsk][ion].index[1:], dfs[dsk][ion].values[1:], 'o-',  color=colors[dsk],label=dsk)
 
 
