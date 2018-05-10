76359a19f7d248805702c8adf6afef24c587aa18
Hilary Egan <hilaryye@gmail.com>
2018-05-10 08:41:53 -0600

    updates to normal/total to make them work w/ derived fields

--- general_functions.py
+++ general_functions.py
@@ -326,14 +326,14 @@ def get_ds_data(ds, field, indx, grid=True, normal=None, ion_velocity=True,
         return get_ds_data(ds, ' electron_pressure', indx, grid=grid, maven=maven)
     elif '_total' in field and field.replace('_total', '_x') in ds.keys():
         vx = get_ds_data(ds, field.replace('_total', '_x'), indx, grid=grid, maven=maven)
-        vx = get_ds_data(ds, field.replace('_total', '_y'), indx, grid=grid, maven=maven)
-        vx = get_ds_data(ds, field.replace('_total', '_z'), indx, grid=grid, maven=maven)
+        vy = get_ds_data(ds, field.replace('_total', '_y'), indx, grid=grid, maven=maven)
+        vz = get_ds_data(ds, field.replace('_total', '_z'), indx, grid=grid, maven=maven)
         return np.sqrt(vx**2+vy**2+vz**2)
     elif '_normal' in field: # field.replace('_normal', '_x') in ds.keys():
 
         vx = get_ds_data(ds, field.replace('_normal', '_x'), indx, grid=grid, maven=maven)
-        vx = get_ds_data(ds, field.replace('_normal', '_y'), indx, grid=grid, maven=maven)
-        vx = get_ds_data(ds, field.replace('_normal', '_z'), indx, grid=grid, maven=maven)
+        vy = get_ds_data(ds, field.replace('_normal', '_y'), indx, grid=grid, maven=maven)
+        vz = get_ds_data(ds, field.replace('_normal', '_z'), indx, grid=grid, maven=maven)
 
         v = np.array([vx,vy,vz])
         vn = np.sum(normal*v, axis=0)
