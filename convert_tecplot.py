5329d913076e87133896ffedfc80682bc04419a1
Hilary Egan <hilaryye@gmail.com>
2020-07-14 16:01:58 -0600

    changing tecplot converter to work w new files and python3

--- convert_tecplot.py
+++ convert_tecplot.py
@@ -37,7 +37,7 @@ f_var_rename = 'Misc/name_conversion.txt'
 def convert_file(fname, h5_name):
 
     # read in file
-    with file(fname) as dat_file: 
+    with open(fname) as dat_file: 
         
         #Process header inforation
         read_header = True
@@ -46,16 +46,30 @@ def convert_file(fname, h5_name):
             l = dat_file.readline()
             
             # Grab the first variable thats at the end of the line
-            if "VARIABLES" in l: dat_vars.append(l.replace("VARIABLES = ", ''))
+            if "VARIABLES" in l: 
+                variables = l.split(',')
+                for var in variables:
+                    var = var.replace("VARIABLES =", '') 
+                    var = var.strip()
+                    dat_vars.append(var)
             
             # If the line begins with " then its another variable
             if l[0] == '"': dat_vars.append(l)
 
             # Contains the number of elements to read
             if "Nodes" in l: N = int( (l.split(',')[0]).split("=")[-1] )
+            if "ZONE T" in l: 
+                metadata = l.split(",")
+                for md in metadata:
+                    md = md.split("=")
+
+                    if md[0].strip() == "N": 
+                        N = int(md[1])
 
             # Begining of last line before data
             if "DT" in l: read_header = False
+
+            if "AUXDATA TIMESIMSHORT" in l: read_header = False
 
         dat_vars = [v.replace('"', '').replace('\n','') for v in dat_vars]
 
@@ -65,7 +79,7 @@ def convert_file(fname, h5_name):
         # Iterate through the data
         for i, line in enumerate(dat_file):
             line_dat = line.split(' ')
-            line_dat = filter(None, line_dat)
+            line_dat = list(filter(None, line_dat))
             for j, key in enumerate(dat_vars):
                 data[key][i] = float(line_dat[j])
 
